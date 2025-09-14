5c60c6049e5fa1f03cbd0491738b62dd42105a46
nina negahdar <bhm577@myy.haaga-helia.fi>
2025-09-14 22:16:38 +0300

    c3.3

--- src/main/java/backend/bookstore/domain/Book.java
+++ src/main/java/backend/bookstore/domain/Book.java
@@ -27,6 +27,14 @@ public class Book {
         this.isbn = isbn;
         this.price = price;
     }
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getTitle() {
         return title;
--- src/main/java/backend/bookstore/web/BookController.java
+++ src/main/java/backend/bookstore/web/BookController.java
@@ -4,8 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import backend.bookstore.domain.Book;
 import backend.bookstore.domain.BookRepository;
 
 @Controller
@@ -23,6 +26,23 @@ private BookRepository bookRepository;
     public String booklist(Model model) {
         model.addAttribute("books", bookRepository.findAll());
         return "booklist";
-
     }
+
+    @RequestMapping(value = "/add")
+    public String addBook(Model model){
+        model.addAttribute("book", new Book());
+        return "addbook";
+    }
+
+    @PostMapping("/add")
+    public String saveBook(Book book) {
+        bookRepository.save(book);
+        return "redirect:/booklist";
+}
+
+    @RequestMapping("/delete/{id}")
+    public String deleteBook(@PathVariable("id") Long id) {
+        bookRepository.deleteById(id);
+        return "redirect:/booklist";
+}
 }
