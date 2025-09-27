e070e89c7700ba1d5cd80ae3ff21b139ed0cc54a
nina negahdar <bhm577@myy.haaga-helia.fi>
2025-09-27 23:05:27 +0300

    log added

--- src/main/java/backend/bookstore/BookstoreApplication.java
+++ src/main/java/backend/bookstore/BookstoreApplication.java
@@ -1,5 +1,7 @@
 package backend.bookstore;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,6 +14,7 @@ import backend.bookstore.domain.CategoryRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
+	private static final Logger log = LoggerFactory.getLogger(BookstoreApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
@@ -20,7 +23,7 @@ public class BookstoreApplication {
 	@Bean
 	public CommandLineRunner demo(BookRepository repository, CategoryRepository categoryRepository) {
 	return (args) -> {
-
+		log.info("Save sample categories:");
 		Category drama = new Category("Drama");
 		Category scifi = new Category("Scifi");
         Category comic = new Category("Comic");
@@ -29,6 +32,7 @@ public class BookstoreApplication {
 		categoryRepository.save(scifi);
 		categoryRepository.save(comic);
 
+		log.info("Save books:");
 		Book b1 = new Book("1984", "George Orwell", 1949, "9780451524935", 9.99);
 		Book b2 = new Book("Brave New World", "Aldous Huxley", 1932, "9780060850524", 8.99);
 		Book b3 = new Book("Fahrenheit 451", "Ray Bradbury", 1953, "9781451673319", 10.99);
@@ -37,7 +41,15 @@ public class BookstoreApplication {
         repository.save(b2);
         repository.save(b3);
 
-	};
-}
+		log.info("Fetch all categories");
+			for (Category category : categoryRepository.findAll()) {
+			log.info(category.toString());
+			}
 
+		log.info("Fetch all books:");
+	        for (Book book : repository.findAll()) {
+	        log.info(book.toString());
+			}
+		};
+	}
 }
